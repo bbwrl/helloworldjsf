3d7160e2f1b15ea1d148aa592ab6995a43ed14f7
Rinaldo Lanza <rinaldo.lanza@bbw.ch>
2018-04-18 13:45:12 +0200

    first commit

---
+++ src/main/java/ch/clip/TestBean.java
@@ -0,0 +1,15 @@
+package ch.clip;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@Named
+@RequestScoped
+public class TestBean {
+	
+	public String getMessage() {
+		return "Hello World";
+	}
+		
+}
